f63fbb30c8ba6cf07fa9b873f20005199511c680
Siluet3656 <Mingaleev02@mail.ru>
2025-03-10 00:08:30 +0400

    enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private Vector3 pos;
+    private Enemy enemyset;
+    public void SpawnAnEnemy(GameObject enemy)
+    {
+        pos = new Vector3(Random.Range(-10f,10f),Random.Range(-10f,10f),0f); 
+        enemyset = Instantiate(enemy, pos, quaternion.identity).GetComponent<Enemy>();
+        enemyset.SetSpeed(SpeedTypeData.GetDataByID((SpeedType)Random.Range(0,2)));
+    }
+}
