d5740ad6e8e9e47eeee2a018613deccc4da0952a
Siluet3656 <Mingaleev02@mail.ru>
2025-07-27 21:52:07 +0400

    pause start

---
+++ Assets/Scripts/GameControl/PausableBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace GameControl
+{
+    public abstract class PausableBehavior : MonoBehaviour
+    {
+        [SerializeField] private bool _pausable;
+
+        private bool _initialized = false;
+
+        protected void Awake()
+        {
+            if (_pausable == false) return;
+            //if ()
+
+            _initialized = true;
+            
+            //enabled
+        }
+
+        private void OnDestroy()
+        {
+            if (_pausable == false) return;
+            if (_initialized == false) return;
+        }
+
+        private void OnPause()
+        {
+            if (_pausable == false) return;
+            enabled = false;
+            PostPause();
+        }
+        
+        private void OnResume()
+        {
+            if (_pausable == false) return;
+            enabled = true;
+            PostResume();
+        }
+        
+        protected virtual void PostPause() {}
+        protected virtual void PostResume() {}
+        protected virtual void CleanUp() {}
+    }
+}
