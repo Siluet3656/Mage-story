d4863b8f80911a41f8167835628b3578fd89a010
Siluet3656 <Mingaleev02@mail.ru>
2025-08-29 16:21:27 +0400

    Straw lairs added and torches now small

---
+++ Assets/Scripts/Effects/Lair.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Effects
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Animator))]
+    public class Lair : MonoBehaviour
+    {
+        [SerializeField] private float _waitTime;
+        
+        private ParticleSystem _particleSystem;
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _particleSystem = GetComponentInChildren<ParticleSystem>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_particleSystem != null)
+            {
+                _particleSystem.Play();
+                _animator.SetBool("blow", true);
+                StopAllCoroutines();
+                StartCoroutine(WaitAnimEnd());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            StopAllCoroutines();
+            _animator.SetBool("blow", false);
+        }
+
+        private IEnumerator WaitAnimEnd()
+        {
+            yield return new WaitForSeconds(_waitTime);
+            _animator.SetBool("blow", false);
+        }
+    }
+}
