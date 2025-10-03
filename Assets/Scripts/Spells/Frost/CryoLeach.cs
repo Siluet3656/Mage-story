32c12402ba880b593915e6fb02b872b86b0607a9
Siluet3656 <Mingaleev02@mail.ru>
2025-10-03 13:45:48 +0400

    Cryo Leach update

--- Assets/Scripts/Spells/Frost/CryoLeach.cs
+++ Assets/Scripts/Spells/Frost/CryoLeach.cs
@@ -1,10 +1,28 @@
-﻿using Data.Enums;
+﻿using UnityEngine;
+using Data.Enums;
 using EnemyStaff;
 
 namespace Spells.Frost
 {
     public class CryoLeach : LineSpell
     {
+        protected override void Awake()
+        {
+            Transform[] allChildren = GetComponentsInChildren<Transform>(includeInactive: true);
+
+            foreach (Transform child in allChildren)
+            {
+                if (child == transform) continue;
+                
+                if (child.CompareTag("CryoLeachEffects"))
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+            
+            base.Awake();
+        }
+        
         public override void DoSpell()
         {
             ApplyDebuff(Target as EnemyTargeting, StatusType.StasisFreeze);
